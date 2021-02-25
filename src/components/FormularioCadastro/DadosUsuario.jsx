a5d7e2519290c87f5716ad8cbe19c2f7b89a7fc1
gmartpad <gabrielmpwork@gmail.com>
2021-02-25 09:40:12 -0300

    primeiro commit

---
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -0,0 +1,32 @@
+import { TextField } from '@material-ui/core'
+import React from 'react'
+import { TextField, Button } from '@material-ui/core';
+
+function DadosUsuario(){
+    return (
+        <form>
+            <TextField 
+                id="email" 
+                label="email" 
+                type="email"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+            <TextField 
+                id="senha" 
+                label="senha" 
+                type="password"
+            />
+            <Button 
+                type="submit"
+                variant="contained"
+                color="primary"
+            >
+                Cadastrar
+            </Button>
+        </form>
+    );
+}
+
+export default DadosUsuario;
